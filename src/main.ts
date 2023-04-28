2bfac60330d7b6d9bfc7b50c1e4aa9bfd3e6835e
LZQ\14529 <1452919179@qq.com>
2023-04-28 21:03:37 +0800

    feat:初始化

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+import { setupStore } from '@/store';
+import 'uno.css';
+
+export function createApp() {
+    const app = createSSRApp(App);
+
+    // Configure store
+    setupStore(app);
+
+    return {
+        app,
+    };
+}
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>333</template>
+
+<style lang="scss" scoped></style>
