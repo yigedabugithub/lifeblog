cff03ea90bc70fbe052aa5e2bc6305776f1ffe9d
LZQ\14529 <1452919179@qq.com>
2023-05-01 15:00:36 +0800

    feat:首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,201 @@
 <script lang="ts" setup>
-const list1 = [
-    'https://cdn.uviewui.com/uview/swiper/swiper1.png',
-    'https://cdn.uviewui.com/uview/swiper/swiper2.png',
-    'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+import { onMounted, ref } from 'vue';
+
+const sysInfo = uni.getSystemInfoSync();
+console.log(sysInfo, 'sysInfo');
+
+// import List from '@/components/List/index.vue';
+// import { ref } from 'vue';
+const list2 = [
+    {
+        image: 'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+        title: '昨夜星辰昨夜风，画楼西畔桂堂东',
+    },
+    {
+        image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+        title: '身无彩凤双飞翼，心有灵犀一点通',
+    },
+
+    {
+        image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+        title: '身无彩凤双飞翼，心有灵犀一点通',
+    },
+    {
+        image: 'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+        title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳',
+    },
 ];
+const fieldList = [
+    {
+        name: '前端',
+        img: '',
+    },
+    {
+        name: '做菜',
+        img: '',
+    },
+    {
+        name: '养宠',
+        img: '',
+    },
+    {
+        name: '前端',
+        img: '',
+    },
+    {
+        name: '做菜',
+        img: '',
+    },
+    {
+        name: '养宠',
+        img: '',
+    },
+];
+
+const urls = [
+    'https://cdn.uviewui.com/uview/album/1.jpg',
+    'https://cdn.uviewui.com/uview/album/2.jpg',
+    'https://cdn.uviewui.com/uview/album/3.jpg',
+    'https://cdn.uviewui.com/uview/album/4.jpg',
+    'https://cdn.uviewui.com/uview/album/5.jpg',
+    'https://cdn.uviewui.com/uview/album/6.jpg',
+    'https://cdn.uviewui.com/uview/album/7.jpg',
+    'https://cdn.uviewui.com/uview/album/8.jpg',
+    'https://cdn.uviewui.com/uview/album/9.jpg',
+    'https://cdn.uviewui.com/uview/album/10.jpg',
+];
+
+// const loadall = (scrollLeft, scrollTop, scrollHeight, scrollWidth, deltaX, deltaY) => {
+//     console.log(scrollLeft, scrollTop, scrollHeight, scrollWidth, deltaX, deltaY, '触发');
+// };
+
+const indexList = ref([] as any);
+const scrolltolower = () => {
+    loadmore();
+};
+
+const loadmore = () => {
+    for (let i = 0; i < 30; i++) {
+        indexList.value.push({
+            url: urls[1],
+        });
+    }
+};
+
+onMounted(() => {
+    loadmore();
+});
 </script>
 
 <template>
-    <u-swiper :list="list1"></u-swiper>
+    <view class="page-home">
+        <view :style="{ height: sysInfo.statusBarHeight + 'px', background: '#fff' }"></view>
+
+        <view class="home-search">
+            <view style="width: 250px">
+                <u-search bgColor="#f5f5f5" :showAction="false" :disabled="true" placeholder="搜索文章"></u-search>
+            </view>
+        </view>
+        <swiper class="swiper-box" circular :indicator-dots="true" :autoplay="true" :interval="2000" :duration="500">
+            <swiper-item v-for="(item, index) in list2" :key="index">
+                <image mode="scaleToFill" :src="item.image"></image>
+            </swiper-item>
+        </swiper>
+        <view class="field-box">
+            <view class="title">
+                <view class="title-tag"></view>
+                我的领域
+            </view>
+            <scroll-view class="my-field" scroll-x="true">
+                <view v-for="(item, index) in fieldList" :key="index" class="my-field-item">
+                    <!-- <image :src="item.img"></image> -->
+                    <view style="display: inline-block">{{ item.name }}</view>
+                </view>
+            </scroll-view>
+        </view>
+        <view class="newarticles-box">
+            <view class="title">
+                <view class="title-tag"></view>
+                热门文章
+            </view>
+
+            <view class="newarticles-list">
+                <u-list @scrolltolower="scrolltolower">
+                    <u-list-item v-for="(item, index) in indexList" :key="index">
+                        <u-cell :title="`列表长度-${index + 1}`">
+                            <template #icon>
+                                <u-avatar shape="square" size="35" :src="item.url" customStyle="margin: -3px 5px -3px 0"></u-avatar>
+                            </template>
+                        </u-cell>
+                    </u-list-item>
+                </u-list>
+            </view>
+        </view>
+    </view>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-home {
+    .home-search {
+        // position: fixed;
+        background-color: #fff;
+        padding: 8px 10px 0;
+    }
+    .swiper-box {
+        height: 220px;
+        padding: 10px;
+        background-color: #fff;
+    }
+    .title {
+        height: 20px;
+        line-height: 20px;
+        padding: 20px 0;
+        display: flex;
+        align-items: center;
+        .title-tag {
+            width: 5px;
+            height: 20px;
+            border-radius: 12px;
+            background-color: #2196f3;
+            margin-right: 10px;
+        }
+    }
+    .field-box {
+        padding: 0 10px;
+
+        .my-field {
+            display: flex;
+            .my-field-item {
+                margin-right: 10px;
+
+                width: 120px;
+                height: 80px;
+                background-color: #fff;
+                text-align: center;
+            }
+        }
+        .my-field {
+            white-space: nowrap;
+            width: 100%;
+        }
+
+        .my-field-item {
+            display: inline-block;
+            width: 150px;
+            height: 60px;
+            line-height: 60px;
+            text-align: center;
+            background-color: #fff;
+            margin-right: 10px;
+        }
+    }
+    .newarticles-box {
+        padding: 0 10px;
+        overflow: hidden;
+        height: calc(100vh - 430px);
+
+        .newarticles-list {
+        }
+    }
+}
+</style>
--- src/pages/notFound/404.vue
+++ src/pages/notFound/404.vue
@@ -1,33 +1,5 @@
-<script lang="ts" setup>
-import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
-import BasicButton from '@/components/BasicButton/index.vue';
-import { useRouter } from '@/hooks/router';
+<script lang="ts" setup></script>
 
-const go = ref<string>('');
-const router = useRouter();
-const redirect = ref<string>('');
-onLoad((query) => {
-    go.value = query.go || '';
-    redirect.value = query.redirect || '';
-});
-
-/**
- * 返回首页
- */
-const backHome = () => {
-    router.pushTab(redirect.value);
-};
-</script>
-
-<template>
-    <view class="w-screen flex flex-col items-center pt-320rpx">
-        <image class="w-360rpx" mode="widthFix" src="@/static/svg/weep.svg" />
-        <view class="mb-40rpx">
-            <text>{{ go }} 页面找不到了~</text>
-        </view>
-        <BasicButton @click="backHome">返回首页</BasicButton>
-    </view>
-</template>
+<template>404</template>
 
 <style lang="scss" scoped></style>
